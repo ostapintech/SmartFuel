b16882a0ee1a6bb2b03b6a51fc91073500cc9225
ostapintech <ostapintech@gmail.com>
2026-05-13 17:30:28 +0300

    update: http access

--- backend/main.py
+++ backend/main.py
@@ -11,10 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"
-                   "http://172.20.10.3:3000"
-                   "http://192.168.1.114:3000"
-                   ], # Адреса фронта твого друга
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
